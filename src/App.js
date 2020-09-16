2b6c113380de0572372e160285bc3721ed930452
pooja.nagalla <pnagalla@gmail.com>
2020-09-16 11:06:13 +0000

    updated src/App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by AbhinavTEST.
+          Edit <code>src/App.js</code> and save to reload by me.
         </p>
 	my_feature	added one more line
          <a
